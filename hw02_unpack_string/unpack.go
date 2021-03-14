13520ecf98b1aba933e6b38bba11beae66b85953
Algis Fateev <algis.fateev@lamoda.ru>
2021-03-14 17:59:37 +0300

    hw02 unpack string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,60 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode/utf8"
+)
+
+const (
+	CharTypeUndefined = iota
+	CharTypeSymbol
+	CharTypeEscape
+	CharTypeNumber
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(encodedString string) (string, error) {
+	if len(encodedString) == 0 {
+		return "", nil
+	}
+	if !utf8.ValidString(encodedString) {
+		return "", ErrInvalidString
+	}
+
+	var b strings.Builder
+	var prevCharValue rune
+	prevCharType := CharTypeUndefined
+	encodedString += " " // last symbol ignored, add one new
+	for i, char := range encodedString {
+		charType, number := parseRune(char)
+		if i == 0 && charType == CharTypeNumber {
+			return "", ErrInvalidString
+		}
+		if charType == CharTypeNumber && prevCharType == CharTypeNumber {
+			return "", ErrInvalidString
+		}
+		if prevCharType == CharTypeEscape {
+			charType = CharTypeSymbol
+		}
+		if prevCharType == CharTypeSymbol {
+			b.WriteString(strings.Repeat(string(prevCharValue), number))
+		}
+
+		prevCharValue = char
+		prevCharType = charType
+	}
+
+	return b.String(), nil
+}
+
+func parseRune(char rune) (int, int) {
+	if char == '\\' {
+		return CharTypeEscape, 1
+	} else if char < '0' || char > '9' {
+		return CharTypeSymbol, 1
+	}
+
+	number := int(char - '0')
+	return CharTypeNumber, number
 }
