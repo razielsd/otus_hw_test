8147210bb73ed43477610e96252aeeb1759e2170
Algis Fateev <algis.fateev@lamoda.ru>
2021-04-14 10:04:06 +0300

    hw04_lru_cache

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,6 +1,7 @@
 package hw04lrucache
 
 import (
+	"fmt"
 	"math/rand"
 	"strconv"
 	"sync"
@@ -49,14 +50,44 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+	t.Run("purge logic - only inserts", func(t *testing.T) {
+		c := NewCache(3)
+		for i := 1; i <= 4; i++ {
+			c.Set(Key(fmt.Sprintf("a%d", i)), 100+i)
+		}
+		_, ok := c.Get("a1")
+		require.False(t, ok, "item must be purged")
+
+		for i := 2; i <= 4; i++ {
+			val, ok := c.Get(Key(fmt.Sprintf("a%d", i)))
+			require.True(t, ok)
+			require.Equal(t, 100+i, val)
+		}
+	})
+
+	t.Run("purge logic - check access", func(t *testing.T) {
+		c := NewCache(3)
+		for i := 1; i <= 3; i++ {
+			c.Set(Key(fmt.Sprintf("a%d", i)), 100+i)
+		}
+		_, ok := c.Get("a1")
+		require.True(t, ok, "first item must be saved")
+
+		c.Set("a4", 104)
+
+		_, ok = c.Get("a2")
+		require.False(t, ok, "second item (key=a2) must be purged after add 4th item")
+
+		keyList := map[Key]int{"a1": 101, "a3": 103, "a4": 104}
+		for k, v := range keyList {
+			cv, ok := c.Get(k)
+			require.True(t, ok, "Purged item with key: "+k+", but must be saved")
+			require.Equal(t, v, cv, "Wrong value from cache")
+		}
 	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
-
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -6,7 +6,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestList(t *testing.T) {
+func TestLinkedList_Complex(t *testing.T) {
+	// внутри тесты не трогал, чтобы было видно что они без изменений
 	t.Run("empty list", func(t *testing.T) {
 		l := NewList()
 
@@ -47,5 +48,197 @@ func TestList(t *testing.T) {
 			elems = append(elems, i.Value.(int))
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+	})
+}
+
+func TestLinkedList_PushFront(t *testing.T) {
+	t.Run("Push front one", func(t *testing.T) {
+		l := NewList()
+		v := 55
+		l.PushFront(v)
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, v, l.Front().Value.(int))
+		require.Equal(t, v, l.Back().Value.(int))
+	})
+
+	t.Run("Push front several", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{10, 20, 30, 40, 50}
+		for _, v := range itemList {
+			l.PushFront(v)
+		}
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[len(itemList)-i-1], v.Value.(int))
+		})
+		require.Equal(t, itemList[len(itemList)-1], l.Front().Value.(int))
+		require.Equal(t, itemList[0], l.Back().Value.(int))
+	})
+}
+
+func TestLinkedList_PushBack(t *testing.T) {
+	t.Run("Push back one", func(t *testing.T) {
+		l := NewList()
+		v := 51
+		l.PushBack(v)
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, v, l.Front().Value.(int))
+		require.Equal(t, v, l.Back().Value.(int))
+	})
+
+	t.Run("Push back several", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		for _, v := range itemList {
+			l.PushBack(v)
+		}
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
+	})
+}
+
+func TestLinkedList_Remove(t *testing.T) {
+	t.Run("Remove single element", func(t *testing.T) {
+		l := NewList()
+		item := l.PushFront(55)
+		l.Remove(item)
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+
+	t.Run("Remove first", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		var rmItem *ListItem
+		rmIndex := 0
+		for i, v := range itemList {
+			item := l.PushBack(v)
+			if i == rmIndex {
+				rmItem = item
+			}
+		}
+		l.Remove(rmItem)
+		itemList = append(itemList[:rmIndex], itemList[rmIndex+1:]...)
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
+	})
+
+	t.Run("Remove last", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		var rmItem *ListItem
+		rmIndex := len(itemList) - 1
+		for i, v := range itemList {
+			item := l.PushBack(v)
+			if i == rmIndex {
+				rmItem = item
+			}
+		}
+		l.Remove(rmItem)
+		itemList = append(itemList[:rmIndex], itemList[rmIndex+1:]...)
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
+	})
+
+	t.Run("Remove middle", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		var rmItem *ListItem
+		rmIndex := 2
+		for i, v := range itemList {
+			item := l.PushBack(v)
+			if i == rmIndex {
+				rmItem = item
+			}
+		}
+		l.Remove(rmItem)
+		itemList = append(itemList[:rmIndex], itemList[rmIndex+1:]...)
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
+	})
+}
+
+func TestLinkedList_MoveToFront(t *testing.T) {
+	t.Run("move to front first", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		var mvItem *ListItem
+		mvIndex := 0
+		for i, v := range itemList {
+			item := l.PushBack(v)
+			if i == mvIndex {
+				mvItem = item
+			}
+		}
+		l.MoveToFront(mvItem)
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
+	})
+
+	t.Run("move to front last", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		var mvItem *ListItem
+		mvIndex := len(itemList) - 1
+		for i, v := range itemList {
+			item := l.PushBack(v)
+			if i == mvIndex {
+				mvItem = item
+			}
+		}
+		l.MoveToFront(mvItem)
+		mvValue := itemList[mvIndex]
+		itemList = append(itemList[:mvIndex], itemList[mvIndex+1:]...)
+		itemList = append([]int{mvValue}, itemList...)
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
+	})
+
+	t.Run("move to front middle", func(t *testing.T) {
+		l := NewList()
+		itemList := []int{60, 70, 80, 90, 100}
+		var mvItem *ListItem
+		mvIndex := 2
+		for i, v := range itemList {
+			item := l.PushBack(v)
+			if i == mvIndex {
+				mvItem = item
+			}
+		}
+		l.MoveToFront(mvItem)
+		mvValue := itemList[mvIndex]
+		itemList = append(itemList[:mvIndex], itemList[mvIndex+1:]...)
+		itemList = append([]int{mvValue}, itemList...)
+		require.Equal(t, len(itemList), l.Len())
+		l.Iterator(func(i int, v *ListItem) {
+			require.Equal(t, itemList[i], v.Value.(int))
+		})
+		require.Equal(t, itemList[0], l.Front().Value.(int))
+		require.Equal(t, itemList[len(itemList)-1], l.Back().Value.(int))
 	})
 }
