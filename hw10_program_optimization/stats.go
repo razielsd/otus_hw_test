317d6b63384296b5696ffc8ce9b9ea12564e7d35
Algis Fateev <algis.fateev@lamoda.ru>
2021-07-13 20:02:06 +0300

    hw10_program_optimization more optimization

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -15,14 +15,13 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
-	extractor := GetHostExtractor(domain)
 	for scanner.Scan() {
 		line := scanner.Bytes()
 		email, err := ExtractEmail(line)
 		if err != nil {
 			return nil, err
 		}
-		host, ok := extractor(email)
+		host, ok := GetHost(domain, email)
 		if !ok {
 			continue
 		}
@@ -35,28 +34,26 @@ func ExtractEmail(line []byte) (string, error) {
 	if len(line) < 1 {
 		return "", errors.New("empty json")
 	}
-	if err := fastjson.ValidateBytes(line); err != nil {
-		return "", err
-	}
 	email := fastjson.GetString(line, "Email")
-	// считаем, что у нас чаще есть поле Email, чем его нету
-	if (email == "") && !fastjson.Exists(line, "Email") {
-		return "", errors.New("not found file Email")
+	// считаем, что у нас чаще json валидный и есть поле Email, чем его нету
+	if email == "" {
+		if err := fastjson.ValidateBytes(line); err != nil {
+			return "", err
+		} else if !fastjson.Exists(line, "Email") {
+			return "", errors.New("not found file Email")
+		}
 	}
 	return email, nil
 }
 
-func GetHostExtractor(domain string) func(email string) (string, bool) {
-	sv := "." + domain
-	return func(email string) (string, bool) {
-		email = strings.ToLower(email)
-		if strings.HasSuffix(email, sv) {
-			info := strings.SplitN(email, "@", 2)
-			if len(info) < 2 {
-				return "", false
-			}
-			return info[1], true
+func GetHost(domain, email string) (string, bool) {
+	email = strings.ToLower(email)
+	if strings.HasSuffix(email, "."+domain) {
+		info := strings.SplitN(email, "@", 2)
+		if len(info) < 2 {
+			return "", false
 		}
-		return "", false
+		return info[1], true
 	}
+	return "", false
 }
