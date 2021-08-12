f50c0802ca6e9f93a9a4ad7aa8cda04ae4298ea4
Algis Fateev <zrandom@mail.ru>
2021-08-12 16:42:42 +0300

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,70 @@ type (
 
 type Stage func(in In) (out Out)
 
+func stageWorker(out Bi, stage Stage, v interface{}) {
+	in := make(Bi)
+	r := stage(in)
+	in <- v
+	sr := <-r
+	close(in)
+	out <- sr
+}
+
+func stagePipe(in In, done In, stage Stage) Bi {
+	out := make(Bi)
+	go func() {
+	L:
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					break L
+				}
+				stageWorker(out, stage, v)
+			case <-done:
+				break L
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+	result := make(Bi)
+	go func() {
+		sin := make(Bi)
+		out := sin
+		for _, stage := range stages {
+			out = stagePipe(out, done, stage)
+		}
+		go func() {
+			// в принципе можно без селекта, но если не закроют канал, тогда горутина так и останется
+			// for v := range in {
+			// 	sin <- v
+			// }
+		L:
+			for {
+				select {
+				case v, ok := <-in:
+					if !ok {
+						break L
+					}
+					sin <- v
+				case <-done:
+					break L
+				}
+			}
+			close(sin)
+		}()
+		for v := range out {
+			result <- v
+		}
+		close(result)
+	}()
+
+	return result
 }
