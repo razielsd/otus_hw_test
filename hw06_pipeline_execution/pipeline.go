1add8df99de1f4c4de77318ff2b4c6458e271ea7
razielsd <zrandom@mail.ru>
2021-08-27 10:03:49 +0300

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,50 +8,34 @@ type (
 
 type Stage func(in In) (out Out)
 
-func stageWorker(out Bi, done In, stage Stage, v interface{}) {
-	in := make(Bi)
-	r := stage(in)
-	select {
-	case in <- v:
-		close(in)
-	case <-done:
-		break
-	}
-	select {
-	case sr := <-r:
-		out <- sr
-	case <-done:
-		break
-	}
-}
-
-func stagePipe(in In, out Bi, done In, stage Stage) {
-L:
-	for {
-		select {
-		case v, ok := <-in:
-			if !ok {
-				break L
+func stagePipe(in In, done In) In {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- v:
+				case <-done:
+					return
+				}
+			case <-done:
+				return
 			}
-			stageWorker(out, done, stage, v)
-		case <-done:
-			break L
 		}
-	}
-	close(out)
+	}()
+	return out
 }
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	if len(stages) == 0 {
-		return in
-	}
-	sin := in
-	var out Bi
 	for _, stage := range stages {
-		out = make(Bi)
-		go stagePipe(sin, out, done, stage)
-		sin = out
+		out := stagePipe(in, done)
+		in = stage(out)
 	}
 
-	return out
+	return in
 }
